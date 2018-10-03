58f9ef02d6fcb9d0c602a2fd78a7ff1398e65a03
Sohail <ahsan.sohail@ibex.co>
2018-10-03 23:16:08 +0500

    old project remove

--- React-Native Sample project/app/Actions/actions.js
+++
@@ -1,8 +0,0 @@
-import * as actionsconstant from '../Constants/actionTypes';
-
-export function setVersion(version) {
-    return {
-        type: actionsconstant.SET_VERSION,
-        payload: version
-      };
-}
--- React-Native Sample project/app/Constants/actionTypes.js
+++
@@ -1,1 +0,0 @@
-export const SET_VERSION = "set_version";
