bdeeebdfd927e1799dfd9ecaf13a101b436651dd
Sohail <ahsan.sohail@ibex.co>
2018-10-09 20:10:04 +0500

    swiper and progress bar

--- MobileApp/src/components/drawer/draweritems.js
+++ MobileApp/src/components/drawer/draweritems.js
@@ -1,9 +1,9 @@
-import ButtonScreen from "../../features/buttonScreen/components/buttonScreen";
-
 const items = [
     {id:1,name:'Home', icon:'md-search', navigateTo: 'app.home'},
     {id:2,name:'Home2', icon: 'md-film', navigateTo: 'app.home2'},
-    {id:3,name:'Button',icon: 'md-film', navigateTo: 'app.buttonlist'}
+    {id:3,name:'Swiper', icon: 'md-film', navigateTo: 'app.swipelist'},
+    {id:4,name:'Progress', icon: 'md-film', navigateTo: 'app.progressbarscreen'},
+    {id:5,name:'Button',icon: 'md-film', navigateTo: 'app.buttonlist'}
 ]
 
 
--- MobileApp/src/navigation/screen_name.js
+++ MobileApp/src/navigation/screen_name.js
@@ -7,6 +7,8 @@ import Login from '../features/login/containers';
 import Signup from '../features/signup/containers';
 import ScreenNew from '../features/screen2/containers';
 import buttonScreen from '../features/buttonScreen/containers'
+import Swiperlist from '../features/swiperlist/containers';
+import ProgressBarScreen from '../features/progressbarexample/containers';
 
 const screens = [
     {name:'global.drawer', component: Drawer},
@@ -17,6 +19,8 @@ const screens = [
     {name:'auth.signup', component: Signup},
     {name:'app.screen2', component: ScreenNew},
     {name:'app.buttonlist',component: buttonScreen},
+    {name:'app.swipelist', component: Swiperlist},
+    {name:'app.progressbarscreen', component: ProgressBarScreen},
 ]
 
 export const registerScreens = (store, provider) => screens.map(screen => Navigation.registerComponent(screen.name, () => screen.component, store, provider))
