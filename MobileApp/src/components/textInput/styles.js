c71841db5fd446e60948fe7fe3b860c9b9b1a789
Dgs-hmzubair <39834631+Dgs-hmzubair@users.noreply.github.com>
2018-10-11 12:56:51 +0500

    TextInput Changes

--- MobileApp/src/components/textInput/styles.js
+++ MobileApp/src/components/textInput/styles.js
@@ -4,12 +4,29 @@ const styles = StyleSheet.create({
 
     searchSection: {
         flexDirection: 'row',
-       
         margin:20
+    },
+    searchSectionWoIcon: {
+        marginRight : 13,
+        alignItems : 'center',
+        padding:15,
+    },
+    searchSectionLeft: {
+        padding:15,
+        paddingRight : 0,
+        paddingLeft : 46,
     },
     searchIcon: {   
         alignItems : 'center',
         padding:15,
+    },
+    searchIconRight: {   
+        alignItems : 'center',
+        padding:15,
+        paddingTop: 18
+    },
+    textInvalid: {   
+        borderColor : 'red'
     },
 
 
--- MobileApp/src/features/textInput/components/textInput/index.js
+++ MobileApp/src/features/textInput/components/textInput/index.js
@@ -6,34 +6,45 @@ import MyTextInput from '../../../../components/textInput';
 export default class TextInputScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = {text: ''};
+    this.state = {text1: '',text2:'',text3:'',text4:''};
     this._onClick = this._onClick.bind(this);
   }
 
-  _onClick = () => {
-    this.setState({text:'Ahsan'})
+  _onClick = (key, value) => {
+    this.setState({[key]: value })
   }
   render(){
     return (
             <View style={{flex:1, justifyContent:'center'}}>
             
               <MyTextInput 
-                backgroundColor = 'brown' 
-                alignSelf = 'center' 
+                // onFocus={() => this._onClick("text1", "Zubair") }
+                // value = {this.state.text1}
+                backgroundColor = 'transparent' 
+                alignSelf = 'center'
+                error = {true}
                 // borderRadius = {30} 
                 iconColor = 'black'
                 inputColor = 'black'
-                placeholderTextColor = 'black' />
+                placeholderTextColor = 'black' 
+                iconName = 'ios-search'
+                iconPos = 'right'/>
               
               <MyTextInput 
+                // onFocus={() => this._onClick("text2", "Ahsan") }
+                // value = {this.state.text2}
                 backgroundColor = 'green' 
                 alignSelf = 'center' 
                 borderRadius = {10} 
                 iconColor = 'black'
                 inputColor = 'black'
+                iconName = 'ios-search'
+
                 placeholderTextColor = 'black' />
 
-                <MyTextInput 
+              <MyTextInput 
+                // onFocus={() => this._onClick("text3", "Zubair1") }
+                // value = {this.state.text3}
                 backgroundColor = 'green' 
                 alignSelf = 'center' 
                 borderRadius = {20} 
@@ -41,7 +52,9 @@ export default class TextInputScreen extends Component {
                 inputColor = 'black'
                 placeholderTextColor = 'black' />
 
-                <MyTextInput 
+              <MyTextInput 
+                // onFocus={() => this._onClick("text4", "Ahsan1") }
+                // value = {this.state.text4}
                 backgroundColor = 'blue' 
                 alignSelf = 'center' 
                 borderRadius = {30} 
