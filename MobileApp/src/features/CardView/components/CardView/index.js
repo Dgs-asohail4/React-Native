7e846757addad800da47480181a6a28751c3cb09
Sohail <ahsan.sohail@ibex.co>
2018-10-16 23:03:21 +0500

    release apk

--- MobileApp/src/features/CardView/components/CardView/index.js
+++ MobileApp/src/features/CardView/components/CardView/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Text, ScrollView } from 'react-native';
 import styles from './styles';
-import img from '../../../../global/assets/img/special.png'
+import img from '../../../../global/assets/img/special.jpg'
 import MyCardView1 from '../../../../components/CardView/Example1';
 import MyCardView2 from '../../../../components/CardView/Example2';
 import MyCardView3 from '../../../../components/CardView/Example3';
@@ -15,30 +15,30 @@ export default class CardView extends Component {
           likesCount = '2000'
           commentsCount = '230'
           sharesCount = '15'
-          image = {img} 
+          image = {img}
         />
         <MyCardView2
           titleText = 'Hello World'
           subtitleText = 'Hello Universe'
           likesCount = '2000'
           commentsCount = '230'
-          sharesCount = '15' 
+          sharesCount = '15'
           image = {img}
         />
-        <MyCardView3 
+        <MyCardView3
           titleText = 'Hello World'
           subtitleText = 'Hello Universe'
           likesCount = '2000'
           commentsCount = '230'
           sharesCount = '15'
-          
+
         />
-        
-        
-        
-        
-        
-        
+
+
+
+
+
+
       </ScrollView>
     );
   }
