6d6cacf1977f6a95c1b129b4e4899dbc5ff30023
Maqbool <Saad.Maqbool@corp.ibexglobal.com>
2018-10-22 12:44:49 +0500

    all list menus

--- MobileApp/src/features/ListViewScreenExample/components/ListViewScreenExample/styles.js
+++ MobileApp/src/features/ListViewScreenExample/components/ListViewScreenExample/styles.js
@@ -1,23 +1,38 @@
 import { StyleSheet } from 'react-native';
 import { COLOR_PRIMARY } from '../../../../global/theme/default';
 
-const styles = StyleSheet.create({
-    container: {
-        marginHorizontal:10,
-        marginTop:5,
-        justifyContent:'center',
-
-     },
-     item:{
-         borderRightWidth:10,
-         borderRightColor:COLOR_PRIMARY,
-         borderLeftWidth:10,
-         height:30,
-         borderRightColor:COLOR_PRIMARY,
-         marginBottom:5,
-         backgroundColor :'#e1e1e1'
-     },
-
-});
-
-export default styles;
+export default class StyleSheetFactory {
+    static getSheet(theme) {
+        return StyleSheet.create({
+            container: {
+                marginHorizontal:10,
+                marginTop:5
+        
+             },
+             item:{
+                
+                 height:50,
+                 marginBottom:30,
+                 marginTop:20,
+                 borderBottomWidth:0.2,
+                 
+                //  backgroundColor :'#e1e1e1'
+             },
+             text: {
+                paddingHorizontal: 16.5,
+                borderBottomWidth: StyleSheet.hairlineWidth,
+                borderColor: theme.colors.border.base,
+              },
+              list: {
+                backgroundColor: theme.colors.screen.base,
+              },
+              emptyContainer: {
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: theme.colors.screen.base,
+              },
+            
+        })
+    }
+  }
--- MobileApp/src/features/ListViewScreenExample/containers/index.js
+++ MobileApp/src/features/ListViewScreenExample/containers/index.js
@@ -4,7 +4,10 @@ import {connect} from 'react-redux'
 import * as actions from '../actions'
 import * as actionsNav from '../../../navigation/actions'
 import { bindActionCreators } from 'redux';
+import { getStyles, getTheme } from '../selectors';
 const mapStateToProps = (state) => ({
+    ...getStyles(state),
+    ...getTheme(state)
 
 });
 const mapDispatchToProps = (dispatch) => ({
