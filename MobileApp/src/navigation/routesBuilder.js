da2182f4a1a911d2ed97cc7c868ff1027fcdad1b
Sohail <ahsan.sohail@ibex.co>
2018-10-23 18:12:42 +0500

    nav bar

--- MobileApp/src/navigation/routesBuilder.js
+++ MobileApp/src/navigation/routesBuilder.js
@@ -5,6 +5,14 @@ import { COLOR_PRIMARY, TEXT_COLOR_PRIMARY } from '../global/theme/default';
 import DrawerIcon from '../components/navIcons/drawerIcon';
 import BackButton from '../components/navIcons/backbutton';
 import EmptyIcon from '../components/navIcons/emptyIcon';
+import NavBar  from '../components/Navbar/container';
+
+
+const renderHeader = (navigation, props,title, drawer) => {
+    return (
+      <NavBar title={title} navigation={navigation} headerProps={props} drawer={drawer}/>
+    );
+};
 
 const generateStack = (routeName, title, showHeader = true, showDrawer = true) => {
     const route = Routes.find( (x) => x.name == routeName);
@@ -13,29 +21,33 @@ const generateStack = (routeName, title, showHeader = true, showDrawer = true) =
         return {
           screen: route.screen,
           title: route.title,
-          navigationOptions: (props) => showHeader ? {
-              headerTitle:route.title,
-              headerStyle:
-                {
-                    color:TEXT_COLOR_PRIMARY,
-                    backgroundColor:COLOR_PRIMARY
-                },
-               headerTitleStyle:{
-                   flex:1,
-                   color:TEXT_COLOR_PRIMARY,
-                   textAlign:'center',
-                   fontSize:28
-               },
-               headerLeft: drawer ? (<DrawerIcon {...props}/>) : (<BackButton {...props}/>),
-               headerRight: (<EmptyIcon />),
-               headerTintColor:TEXT_COLOR_PRIMARY
-            } : {
-              header:null
-          }
+          navigationOptions: ({ navigation }) => showHeader ? ({
+            gesturesEnabled: false,
+            headerTitle:route.title,
+            header: (props) => renderHeader(navigation, props, route.title, drawer),
+          }) : ({header:null})
+          //{
+            //   headerTitle:route.title,
+            //   headerStyle:
+            //     {
+            //         color:TEXT_COLOR_PRIMARY,
+            //         backgroundColor:COLOR_PRIMARY
+            //     },
+            //    headerTitleStyle:{
+            //        flex:1,
+            //        color:TEXT_COLOR_PRIMARY,
+            //        textAlign:'center',
+            //        fontSize:28
+            //    },
+            //    headerLeft: drawer ? (<DrawerIcon {...props}/>) : (<BackButton {...props}/>),
+            //    headerRight: (<EmptyIcon />),
+            //    headerTintColor:TEXT_COLOR_PRIMARY
+            // }
+
         }
     };
 
-    flatRoutes[route.name] = wrapToRoute(route, true);
+    flatRoutes[route.name] = wrapToRoute(route, route.name.contains("menu") ? false : true );
     for (let child of route.childrens) {
         flatRoutes[child.name] = wrapToRoute(child);
     }
