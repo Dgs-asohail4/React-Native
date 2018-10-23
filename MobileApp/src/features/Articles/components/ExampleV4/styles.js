99eca73cdd793815deaf41bfcd829a73c61d0009
Maqbool <Saad.Maqbool@corp.ibexglobal.com>
2018-10-23 19:44:38 +0500

    changes in article V3

--- MobileApp/src/features/Articles/components/ExampleV4/styles.js
+++ MobileApp/src/features/Articles/components/ExampleV4/styles.js
@@ -2,6 +2,20 @@ import { StyleSheet } from 'react-native';
 export default class StyleSheetFactory {
     static getSheet(theme) {
         return StyleSheet.create({
+            container: {
+                backgroundColor: theme.colors.screen.scroll,
+                paddingHorizontal: 14,
+                paddingVertical: 8,
+              },
+              card: {
+                marginVertical: 8,
+              },
+              footer: {
+                paddingTop: 16,
+              },
+              time: {
+                marginTop: 5,
+              },
         })
     }
 };
