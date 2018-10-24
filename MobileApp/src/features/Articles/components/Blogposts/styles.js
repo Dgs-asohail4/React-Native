64682246c1b4cad8ee59ad585e3faaad51d1ecf2
Maqbool <Saad.Maqbool@corp.ibexglobal.com>
2018-10-24 16:41:44 +0500

    blogpost and article list is completed without styling

--- MobileApp/src/features/Articles/components/Blogposts/styles.js
+++ MobileApp/src/features/Articles/components/Blogposts/styles.js
@@ -2,6 +2,21 @@ import { StyleSheet } from 'react-native';
 export default class StyleSheetFactory {
     static getSheet(theme) {
         return StyleSheet.create({
+            container: {
+                backgroundColor: theme.colors.screen.scroll,
+                paddingVertical: 8,
+                paddingHorizontal: 14,
+              },
+              card: {
+                marginVertical: 8,
+              },
+              userInfo: {
+                flexDirection: 'row',
+                alignItems: 'center',
+              },
+              avatar: {
+                marginRight: 17,
+              },
         })
     }
 };
--- MobileApp/src/features/Articles/components/ExampleV4/styles.js
+++ MobileApp/src/features/Articles/components/ExampleV4/styles.js
@@ -2,20 +2,17 @@ import { StyleSheet } from 'react-native';
 export default class StyleSheetFactory {
     static getSheet(theme) {
         return StyleSheet.create({
-            container: {
-                backgroundColor: theme.colors.screen.scroll,
-                paddingHorizontal: 14,
-                paddingVertical: 8,
-              },
-              card: {
-                marginVertical: 8,
-              },
-              footer: {
-                paddingTop: 16,
-              },
-              time: {
-                marginTop: 5,
-              },
-        })
+          container: {
+            backgroundColor: theme.colors.screen.scroll,
+            paddingVertical: 8,
+            paddingHorizontal: 14,
+          },
+          card: {
+            marginVertical: 8,
+          },
+          post: {
+            marginTop: 13,
+          },
     }
+        )}
 };
