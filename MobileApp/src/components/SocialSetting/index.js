d3baa0c3078dbea2dc990d20b37c10c9f03388f6
Sohail <ahsan.sohail@ibex.co>
2018-10-25 21:43:01 +0500

    switch component

---
+++ MobileApp/src/components/SocialSetting/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+import StyleSheetFactory from './styles';
+import Theme from '../../global/theme'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import { DEFUALT_THEME } from '../../global/config';
+
+import Switch from '../Switch/container'
+
+export default class SocialSetting extends Component {
+
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    icon: PropTypes.node.isRequired,
+    selected: PropTypes.bool,
+    tintColor: PropTypes.string,
+  };
+
+  static defaultProps = {
+    selected: true,
+    tintColor: Theme[DEFUALT_THEME].colors.accent,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: this.props.selected,
+    };
+  }
+
+  onSwitchValueChanged = (value) => {
+    this.setState({ selected: value });
+  };
+
+  render() {
+    const color = this.state.selected ? this.props.tintColor : Theme[this.props.theme].colors.disabled;
+    const styles = StyleSheetFactory.getSheet(Theme[this.props.theme])
+    const {awesome,large,small,baseColor} = this.props.globalStyles.textStyle;
+    return (
+      <View style={styles.container}>
+        <View style={styles.left}>
+          <Text style={[awesome,large, styles.icon, {color}]}>
+            <Icon name={this.props.icon} size={20} color={color} />
+          </Text>
+          <Text style={[small,{color}]}>{this.props.name}</Text>
+        </View>
+
+
+        <Switch theme={this.props.theme} value={this.state.selected} onValueChange={this.onSwitchValueChanged} />
+      </View>
+    );
+  }
+}
---
+++ MobileApp/src/global/styles/inputStyle.js
@@ -0,0 +1,52 @@
+
+import {StyleSheet} from 'react-native';
+import {scale} from '../../utils/scale'
+export default class StyleSheetFactory {
+  static getSheet(theme) {
+      const inputStyle = StyleSheet.create({
+          basic:{
+              fontFamily :theme.fonts.family.bold
+          },
+          square:{
+              borderRadius:3,
+              justifyContent:'center',
+              backgroundColor:theme.colors.button.back,
+          }, //container : {flexDirection:'column',margin:8}
+          tile:{
+              borderRadius:0,
+              backgroundColor:'transparent',
+              borderWidth:0.5,
+              justifyContent:'center',
+              borderColor:theme.colors.border.base,
+          }, //container: {flexDirection:'column'}
+          social:{
+            height: scale(62),
+            width: scale(62),
+            borderRadius: scale(31),
+            borderColor: theme.colors.border.accent,
+            borderWidth: 1,
+            backgroundColor: theme.colors.control.background,
+          },
+          link:{
+              color:theme.colors.primary
+          },
+          contast:{
+              color: theme.colors.text.base
+          },
+          icon:{
+              height:scale(56),
+              width:scale(56),
+              borderColor:theme.colors.border.base,
+              backgroundColor:theme.colors.control.background,
+              borderWidth:1
+          },
+          highlight:{
+              backgroundColor:theme.colors.button.highlight
+          }
+
+
+      });
+      return inputStyle;
+  }
+}
+
