1d7e089319b64b762f5e3799af514757932072bb
Sohail <ahsan.sohail@ibex.co>
2018-10-29 12:09:11 +0500

    Dashboard Screen

--- MobileApp/src/features/login/components/login/styles.js
+++ MobileApp/src/features/login/components/login/styles.js
@@ -3,16 +3,6 @@ import {scaleVertical} from '../../../../utils/scale';
 export default class StyleSheetFactory {
   static getSheet(theme) {
   return StyleSheet.create({
-    input: {
-        width: 350,
-        fontSize: 18,
-        fontWeight: '500',
-        height: 55,
-        backgroundColor: '#42A5F5',
-        color: 'white',
-        padding: 8,
-        borderRadius: 24
-      },
       container: {
         paddingHorizontal: 17,
         paddingBottom: scaleVertical(22),
