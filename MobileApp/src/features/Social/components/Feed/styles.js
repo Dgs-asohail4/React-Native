94437749ebf7af9c200968d054eaae01cd20dab9
Maqbool <Saad.Maqbool@corp.ibexglobal.com>
2018-10-31 14:42:50 +0500

    social tab completed with a little bit issues in contact and notification

--- MobileApp/src/features/Social/components/Feed/styles.js
+++ MobileApp/src/features/Social/components/Feed/styles.js
@@ -2,6 +2,17 @@ import { StyleSheet } from 'react-native';
 export default class StyleSheetFactory {
     static getSheet(theme) {
         return StyleSheet.create({
+            container: {
+                backgroundColor: theme.colors.screen.scroll,
+                paddingVertical: 8,
+                paddingHorizontal: 10,
+              },
+              card: {
+                marginVertical: 8,
+              },
+              avatar: {
+                marginRight: 16,
+              },
         })
     }
 };
