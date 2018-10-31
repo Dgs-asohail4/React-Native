858175ea9e2af1b1f24575837970ce176e3c7c92
Sohail <ahsan.sohail@ibex.co>
2018-10-31 20:34:05 +0500

    push notification

--- MobileApp/src/features/PushNotificationExample/containers/index.js
+++ MobileApp/src/features/PushNotificationExample/containers/index.js
@@ -1,14 +1,19 @@
-
-import PushNotificationExample from '../components/PushNotificationExample'
+import PushNotification from '../components/PushNotificationExample'
 import {connect} from 'react-redux'
 import * as actions from '../actions'
 import * as actionsNav from '../../../navigation/actions'
-import { bindActionCreators } from 'redux';
-const mapStateToProps = (state) => ({
+import * as actionSplash from '../../splash/actions'
 
+import { bindActionCreators } from 'redux';
+import { getTheme, getStyles } from '../selectors';
+
+const mapStateToProps = (state) => ({
+...getTheme(state),
+...getStyles(state)
 });
 const mapDispatchToProps = (dispatch) => ({
-    ...bindActionCreators(actions, dispatch),
-    ...bindActionCreators(actionsNav,dispatch)
+...bindActionCreators(actions, dispatch),
+...bindActionCreators(actionsNav,dispatch),
+...bindActionCreators(actionSplash,dispatch)
 });
-export default connect(mapStateToProps, mapDispatchToProps)(PushNotificationExample);
+export default connect(mapStateToProps, mapDispatchToProps)(PushNotification);
