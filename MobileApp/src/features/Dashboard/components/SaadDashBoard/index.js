e99d7aef293620dfd7cef398fef3ca640e91a53f
Maqbool <Saad.Maqbool@corp.ibexglobal.com>
2018-11-06 14:39:42 +0500

    pie chart and stack bar chart completed

--- MobileApp/src/features/Dashboard/components/SaadDashBoard/index.js
+++ MobileApp/src/features/Dashboard/components/SaadDashBoard/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import PieChartExample from '../../../../components/pieChartExample/container'
+import StackedBarChartExample from '../../../../components/StackedBarChartExample/container'
 import { Text, View } from 'react-native';
+
 import StyleSheetFactory from './styles';
 import Theme from '../../../../global/theme'
 
@@ -8,8 +11,12 @@ export default class DashBoardV3 extends Component {
     const styles = StyleSheetFactory.getSheet(Theme[this.props.theme]);
     return (
       <View>
-        <Text>Title</Text>
+
+        <PieChartExample />
+        <StackedBarChartExample />
       </View>
-    );
+      
+          );
   }
+  
 }
